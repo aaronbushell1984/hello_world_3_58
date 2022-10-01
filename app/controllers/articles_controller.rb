34b404ebafeb268aee4c281b2dbbf3ce3fc7b4c2
Aaron <aaron_bushell@yahoo.com>
2022-10-02 00:51:34 +0100

    Add binding.break debug example and commented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   def show
+    # used to debug - left as example learning code
+    # binding.break
     @article = Article.find(params[:id])
   end
 end
